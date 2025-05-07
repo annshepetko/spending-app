81c57862b4e901139b8881850996e5d346f4eb82
andriishepetko <ivan.ssh07@gmail.com>
2025-05-08 01:16:48 +0300

    hibernate config

--- src/main/java/com/ann/spending/SpendingApplication.java
+++ src/main/java/com/ann/spending/SpendingApplication.java
@@ -2,9 +2,11 @@ package com.ann.spending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
+@EnableJpaRepositories
 public class SpendingApplication {
 
 	public static void main(String[] args) {
