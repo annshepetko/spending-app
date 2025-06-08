e4bf01bb2b09179b7ec3f2c650b0069e771b0c14
andriishepetko <ivan.ssh07@gmail.com>
2025-06-08 22:52:55 +0300

    fix shopping category icon

--- src/main/resources/db/migration/V2__insertion_into_basic_categoies.sql
+++ src/main/resources/db/migration/V2__insertion_into_basic_categoies.sql
@@ -5,7 +5,7 @@ INSERT INTO category(category_id, name, icon_name)
 VALUES (nextval('category_seq_gen'), 'Bills', 'billsIcon'),
        (nextval('category_seq_gen'), 'Transport', 'airplaneIcon'),
        (nextval('category_seq_gen'), 'Mobile', 'mobileIcon'),
-       (nextval('category_seq_gen'), 'Shopping', ' shoppingIcon'),
+       (nextval('category_seq_gen'), 'Shopping', 'shoppingIcon'),
        (nextval('category_seq_gen'), 'Insurance', 'healthIcon'),
        (nextval('category_seq_gen'), 'Voucher', 'percentageIcon'),
        (nextval('category_seq_gen'), 'Internet', 'internetIcon'),
